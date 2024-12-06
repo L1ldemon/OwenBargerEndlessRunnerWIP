9b479074c20c2a1cd57779736a7535837851be57
Owen <owenb2528@gmail.com>
2024-12-06 10:04:34 -0500

    doing the difficulty script

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Difficulty : MonoBehaviour
 {
+    [SerializeField] GameObject Player;
+
+    [SerializeField] List <GameObject> player_spawns = new List <GameObject> {};
+    
     [SerializeField] bool easy;
+    
     [SerializeField] bool medium;
+    
     [SerializeField] bool hard;
+    
     [SerializeField] bool insane;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +24,35 @@ public class Difficulty : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(easy == true)
+        {
+            medium = false;
+            hard = false;
+            insane = false;
+            Vector3 position = new Vector3 (Player 0.96.transform.position.x, Player -1.66.transform.position.y, Player 0.transform.position.z);
+            Debug.Log("Easy Mode selected");
+        }
+    if(medium == true)
+        {
+            easy = false;
+            hard = false;
+            insane = false;
+            Vector3 position = new Vector3 (Player.transform.position.x, Player.transform.position.y, Player.transform.position.z);
+            Debug.Log("Medium Mode selected");
+        }
+    if(hard == true)
+        {
+            medium = false;
+            easy = false;
+            insane = false;
+            Debug.Log("Hard Mode selected");
+        }
+    if(insane == true)
+        {
+            medium = false;
+            hard = false;
+            easy = false;
+            Debug.Log("Insane Mode selected");
+        }
     }
 }
