11d6e27b00a36b7724008f33b4c993419737373c
Owen <owenb2528@gmail.com>
2024-12-16 09:59:39 -0500

    Added a restart button

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
     public bool isAlive;
+    public int restart;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,10 @@ public class GameOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.R))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(restart);
+        }
     }
 }
